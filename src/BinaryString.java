8ff80982ed717b48737c93610605418308d62acc
mayank8807 <jainmayank8807@gmail.com>
2019-05-06 03:28:57 +0900

    Bianry , K-ary string
    adding recursive sloution to generate binary and K-ary string

---
+++ src/BinaryString.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class BinaryString {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please input lenght of string");
+		int size = sc.nextInt();
+		genrateString("",size);  // Initializing result string as empty
+	}
+	
+	public static void genrateString(String sGenerated,int length)
+	{
+		//base case 
+		if(length==1)
+		{
+			//for binary 2 values are possible at base case either 1 or 0
+			System.out.println(sGenerated+0);   // adding 0 as base case and print 
+			System.out.println(sGenerated+1);  //  adding 1 as base case and print
+		}
+		else
+		{
+			genrateString(sGenerated+0,length-1);  //add one possible value 0 and send for recursion
+			genrateString(sGenerated+1,length-1);  // add one possible value 1 and send for recursion
+		}
+	}
+
+}
---
+++ src/K_aryString.java
@@ -0,0 +1,38 @@
+//This program all possible string for given length using values between 0 , 1 ...K
+//Recursion is used to generate output.
+// base case will be string of length 1 , when we will have string for each possible value.
+import java.util.Scanner;
+
+public class K_aryString {
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+	
+		// getting possible value of K
+		System.out.println("Enter the vlaue of K ( for given value of K , String will be generated using 0- K);");
+		int k = sc.nextInt();
+		// getting size of string which need to be generated
+		System.out.println("Please provide length of String which need to be generated:");
+		int length = sc.nextInt();
+		generate("",length,k);
+	}
+	
+	public static  void generate(String sGegerated, int length, int k)
+	{
+		//base case
+		if(length<1)
+		{
+				System.out.println(sGegerated);  
+		}
+		
+		else
+		{
+			for(int i=0; i<=k;i++)// add all possible values of K
+			{
+				generate(sGegerated+i,length-1,k);
+			}
+		}
+	}
+
+}
